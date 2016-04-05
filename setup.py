c1184a795b615642533f8b7128eabb27e4ea1d99
Ruben Verweij <rbnvrw@gmail.com>
2016-04-05 13:33:49 +0200

    Convert to package for easy installation and running

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+try:
+    descr = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+except IOError:
+    descr = ''
+
+try:
+    from pypandoc import convert
+
+    descr = convert(descr, 'rst', format='md')
+except ImportError:
+    pass
+
+setup_parameters = dict(
+    name="pimsviewer",
+    version='0.1',
+    description="PIMS viewer",
+    author="Casper van der Wel",
+    author_email="caspervdw@gmail.com",
+    url="https://bitbucket.org/caspervdw/pimsviewer",
+    install_requires=['scikit-image>=0.9', 'matplotlib', 'pims'],
+    packages=['pimsviewer'],
+    long_description=descr,
+)
+
+setup(**setup_parameters)
