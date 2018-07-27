c138c52e689f531296b0c35441b5bbd844457e85
Ruben Verweij <verweij@physics.leidenuniv.nl>
2018-07-27 14:41:08 +0200

    Fix playing of z + dpi values

--- pimsviewer/viewer.py
+++ pimsviewer/viewer.py
@@ -11,14 +11,17 @@ except ImportError: # Python 2
 import pygubu
 from tkinter.filedialog import askopenfilename
 import matplotlib as mpl
+
+mpl.use("TkAgg")
+
 import matplotlib.backends.tkagg as tkagg
-from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import pims
 
 from nd2reader import ND2Reader
 
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
-dpi = 300
+fps = 5
 
 class Viewer:
     def __init__(self, filename, reader_class):
@@ -30,6 +33,7 @@ class Viewer:
         
         # 3: Create the toplevel widget.
         self.mainwindow = builder.get_object('Toplevel_1')
+        self._dpi = self.mainwindow.winfo_fpixels('1i')
         self.frame = builder.get_object('Frame_1')
         self.frame.pack(fill=tk.BOTH, expand=True)
 
@@ -42,7 +46,8 @@ class Viewer:
         # 5: Program specific code
         self.filename = filename
         self.figure, self.ax = self.create_figure()
-        self.canvas = builder.get_object('Canvas_1')
+        self.canvas_frame = builder.get_object('CanvasFrame')
+        self.canvas = self.init_canvas()
         self.photo = None
         self.reader = None
 
@@ -52,6 +57,7 @@ class Viewer:
         self.sliders = {}
         self.configure_sliders()
         self._playing = None
+        self._play_job = None
 
         # 7: Connect callback functions
         builder.connect_callbacks(self)
@@ -62,7 +68,7 @@ class Viewer:
             self.open_file()
 
     def create_figure(self):
-        fig = mpl.figure.Figure(figsize=(200/dpi, 200/dpi), dpi=dpi)
+        fig = mpl.figure.Figure(figsize=(200/self._dpi, 200/self._dpi), dpi=self._dpi)
         ax = fig.add_axes([0, 0, 1, 1])
         ax.axis('off')
         return fig, ax
@@ -86,14 +92,23 @@ class Viewer:
     def show_frame(self):
         frame_no = self.sliders['t']['current'] - 1
         channel_no = self.sliders['c']['current'] - 1
+        z_no = self.sliders['z']['current'] - 1
 
         if not hasattr(self.reader, 'sizes'):
             self.reader.sizes = {}
         if 'c' in self.reader.sizes:
-            self.reader.bundle_axes = 'cyx'
-            self.ax.imshow(self.reader[frame_no][channel_no])
+            if 'z' in self.reader.sizes:
+                self.reader.bundle_axes = 'czyx'
+                self.ax.imshow(self.reader[frame_no][channel_no, z_no, :, :])
+            else:
+                self.reader.bundle_axes = 'cyx'
+                self.ax.imshow(self.reader[frame_no][channel_no, :, :])
         else:
-            self.ax.imshow(self.reader[frame_no])
+            if 'z' in self.reader.sizes:
+                self.reader.bundle_axes = 'zyx'
+                self.ax.imshow(self.reader[frame_no][z_no, :, :])
+            else:
+                self.ax.imshow(self.reader[frame_no])
         self.draw_figure()
 
     def on_slider_change(self, event=None):
@@ -140,7 +155,7 @@ class Viewer:
                     try:
                         self.sliders[prop]['fps'] = self.reader.frame_rate
                     except AttributeError:
-                        self.sliders[prop]['fps'] = 1
+                        self.sliders[prop]['fps'] = fps
 
 
     def hide_all_sliders(self):
@@ -154,7 +169,7 @@ class Viewer:
                 'play_btn': self.builder.get_object('ChannelPlayBtn'),
                 'slider': self.builder.get_object('ChannelScale'),
                 'current': 1,
-                'fps': 1
+                'fps': fps
         }
 
         self.sliders['t'] = {
@@ -163,7 +178,7 @@ class Viewer:
                 'play_btn': self.builder.get_object('TimePlayBtn'),
                 'slider': self.builder.get_object('TimeScale'),
                 'current': 1,
-                'fps': 1
+                'fps': fps
         }
 
         self.sliders['z'] = {
@@ -172,7 +187,7 @@ class Viewer:
                 'play_btn': self.builder.get_object('ZPlayBtn'),
                 'slider': self.builder.get_object('ZScale'),
                 'current': 1,
-                'fps': 1
+                'fps': fps
         }
 
         self.hide_all_sliders()
@@ -191,18 +206,16 @@ class Viewer:
         self.statusbar.configure(text=text)
         self.statusbar.update()
 
-    def draw_figure(self):
-        cpw = self.canvas.winfo_width()
-        cph = self.canvas.winfo_height()
-        self.figure.set_size_inches(cpw/dpi, cph/dpi, forward=True)
-        figure_canvas_agg = FigureCanvasAgg(self.figure)
-        figure_canvas_agg.draw()
-        figure_x, figure_y, figure_w, figure_h = self.figure.bbox.bounds
-        figure_w, figure_h = int(figure_w), int(figure_h)
-        self.photo = tk.PhotoImage(master=self.canvas, width=figure_w, height=figure_h)
+    def init_canvas(self):
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self.canvas_frame)
+        self.canvas.draw_idle()
+        self.canvas.get_tk_widget().grid(row=0, column=0, pady=0, padx=0, sticky='nsew')
+        return self.canvas
 
-        self.canvas.create_image(figure_w/2, figure_h/2, image=self.photo)
-        tkagg.blit(self.photo, figure_canvas_agg.get_renderer()._renderer, colormode=2)
+    def draw_figure(self):
+        cpw, cph = self.canvas.get_width_height()
+        self.figure.set_size_inches(cpw/self._dpi, cph/self._dpi, forward=True)
+        self.canvas.draw_idle()
 
     def set_accelerators(self):
         self.mainwindow.bind_all("<Control-q>", self.quit)
@@ -211,37 +224,46 @@ class Viewer:
     def resize(self, event=None):
         self.draw_figure()
 
-    def toggle_play_channel(self):
-        print('playing channel')
-
     def toggle_play_time(self):
+        self.toggle_play('t')
+
+    def toggle_play_channel(self):
+        self.toggle_play('c')
+
+    def toggle_play_z(self):
+        self.toggle_play('z')
+
+    def toggle_play(self, prop):
         if self._playing is None:
-            self._playing = 't'
-            self.sliders['t']['play_btn'].configure(text='⏸')
-            self.sliders['t']['play_btn'].update()
-            timeout = int(round(1.0 / self.sliders['t']['fps'] * 1000.0))
-            self.update_statusbar("Playing axis 't' @ %.1f FPS" % (self.sliders['t']['fps']))
-            self.mainwindow.after(timeout, self.play_time, timeout)
-        elif self._playing is 't':
-            self.sliders['t']['play_btn'].configure(text='⏵')
-            self.sliders['t']['play_btn'].update()
+            self._playing = prop
+            self.sliders[prop]['play_btn'].configure(text='⏸')
+            self.sliders[prop]['play_btn'].update()
+            timeout = int(round(1.0 / self.sliders[prop]['fps'] * 1000.0))
+            self.update_statusbar("Playing axis '%s' @ %.1f FPS" % (prop, self.sliders[prop]['fps']))
+            if self._play_job:
+                self.mainwindow.after_cancel(self._play_job)
+            self._play_job = self.mainwindow.after(timeout, self.play, timeout, prop)
+        elif self._playing is prop:
+            if self._play_job:
+                self.mainwindow.after_cancel(self._play_job)
+            self.sliders[prop]['play_btn'].configure(text='⏵')
+            self.sliders[prop]['play_btn'].update()
             self.update_statusbar()
             self._playing = None
 
-    def toggle_play_z(self):
-        print('playing z')
-
-    def play_time(self, timeout):
-        if self._playing is None or self._playing != 't':
+    def play(self, timeout, prop):
+        if self._playing is None or self._playing != prop:
             return
 
-        new_value = self.sliders['t']['current'] + 1
-        if new_value > self.reader.sizes['t']:
+        if self._play_job:
+            self.mainwindow.after_cancel(self._play_job)
+
+        new_value = self.sliders[prop]['current'] + 1
+        if new_value > self.reader.sizes[prop]:
             new_value = 1
-        self.sliders['t']['slider'].set(new_value)
+        self.sliders[prop]['slider'].set(new_value)
         self._on_slider_change(override_playing=True)
 
         self.show_frame()
 
-        self.mainwindow.after(timeout, self.play_time, timeout)
-
+        self._play_job = self.mainwindow.after(timeout, self.play, timeout, prop)
